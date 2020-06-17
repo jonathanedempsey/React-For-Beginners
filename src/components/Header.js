a19f7e1d982a11ef44d05ad9d599cd51f255cc2d
Jonathan Dempsey <jonathanedempsey@gmail.com>
2020-06-17 18:22:26 +0100

    Creating our application layour with components
    Finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,18 @@ class Header extends React.Component {
     render() {
         return (
             <header className="top">
-                <h1>Catch of the Day</h1>
+                <h1>
+                    Catch
+                    <span class="ofThe">
+                        <span className="of">Of</span>
+                        <span className="the">The</span>
+                    </span>
+                    Day
+                </h1>
                 <h3 className="tagline">
-                    <span>Fresh Daily</span>
+                    <span>Fresh Seafood Market</span>
                 </h3>
             </header>
-
         )
     }
 }
---
+++ src/components/Order.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Order extends React.Component {
+    render() {
+        return (
+            <div className="prder">Order!!!</div>
+        )
+    }
+}
+
+export default Order;
