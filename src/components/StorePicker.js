2e652e62d628882b46c1c9c998d3f9481c4d5078
Jonathan Dempsey <jonathanedempsey@gmail.com>
2020-06-25 17:58:31 +0100

    Events, Refs and this Binding

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,13 +2,26 @@ import React, { Fragment } from 'react';
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+    goToStore(event) {
+        // 1. Stop form from submitting
+        event.preventDefault();
+
+        // 2. Get text from input
+        // 3. Change the page to /store/what-ever-they-enetered
+    }
     render() {
         return (
             <Fragment>
-                { /* I'm a comment! */}
-                <form className="store-selector">
+                { /* goToStore - we dont include () because we dont want it to run on page load */ }
+                { /* getFunName() - we include () because we DO want it to run on page load */ }
+                <form className="store-selector" onSubmit={this.goToStore}>
                     <h2>Please Enter A Store</h2>
-                    <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+                    <input
+                        type="text"
+                        required
+                        placeholder="Store Name"
+                        defaultValue={getFunName()}
+                    />
                     <button type="submit">Visit Store</button>
                 </form>
             </Fragment>
