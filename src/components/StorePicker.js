1200d8dcb13dc034acbe92a551ae2e42b067d31d
Jonathan Dempsey <jonathanedempsey@gmail.com>
2020-06-25 18:11:43 +0100

    Events, Refs and this Binding

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,29 @@ import React, { Fragment } from 'react';
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-    goToStore(event) {
+    myInput = React.createRef();
+
+    // Option 1 : Since goToStore isn't bound in React by default (because it's custom made by us),
+    // we need to bind it so 'this' will work in our StorePicker class
+    // constructor() {
+    //     super();
+    //     console.log("Run");
+    //     this.goToStore = this.goToStore.bind(this);
+    // }
+    // goToStore(event) {
+    //     code here..
+    // }
+
+
+    // Option 2: the arrow function will be bound to the instance automatically so it's
+    // which also allows us to use 'this'. Much cleaner than option 1
+    goToStore = (event) => {
         // 1. Stop form from submitting
         event.preventDefault();
 
         // 2. Get text from input
+        console.log(this.myInput);
+
         // 3. Change the page to /store/what-ever-they-enetered
     }
     render() {
@@ -18,6 +36,7 @@ class StorePicker extends React.Component {
                     <h2>Please Enter A Store</h2>
                     <input
                         type="text"
+                        ref={this.myInput}
                         required
                         placeholder="Store Name"
                         defaultValue={getFunName()}
