a085f8dbfea1614d50d182c9e7641aa4246b6451
Jonathan Dempsey <jonathanedempsey@gmail.com>
2020-07-02 18:11:40 +0100

    Understanding State
    Creating refs for form elements

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,17 +1,35 @@
 import React from "react";
 
 class AddFishForm extends React.Component {
+    nameRef = React.createRef();
+    priceRef = React.createRef();
+    statusRef = React.createRef();
+    descRef = React.createRef();
+    imageRef = React.createRef();
+
+    createFish = (event) => {
+        event.preventDefault();
+        const fish = {
+            name: this.nameRef.current.value,
+            price: parseFloat(this.priceRef.current.value),
+            status: this.statusRef.current.value,
+            desc: this.descRef.current.value,
+            image: this.imageRef.current.value
+          };
+        console.log(fish);
+    };
+
     render() {
         return (
-            <form className="fish-edit">
-                <input name="name" type="type" placeholder="Name" />
-                <input name="price" type="type" placeholder="Price" />
-                <select name="status">
+            <form className="fish-edit" onSubmit={this.createFish}>
+                <input name="name" ref={this.nameRef} type="type" placeholder="Name" />
+                <input name="price" ref={this.priceRef} type="type" placeholder="Price" />
+                <select name="status" ref={this.statusRef}>
                     <option value="available">Fresh</option>
                     <option value="unavailable">Sold Out</option>
                 </select>
-                <textarea name="desc" placeholder="Desc" />
-                <input name="image" type="type" placeholder="Image" />
+                <textarea name="desc" ref={this.descRef} placeholder="Desc" />
+                <input name="image" ref={this.imageRef} type="type" placeholder="Image" />
                 <button type="submit">+ Add Fish</button>
             </form>
         );
