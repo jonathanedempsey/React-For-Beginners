82bdc4534cac991d7da59362de9d2b523b36c86c
Jonathan Dempsey <jonathanedempsey@gmail.com>
2020-07-26 13:34:01 +0100

    Displaying Order State with JSX
    Passing order ids into our order state

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,8 +2,16 @@ import React from "react";
 
 class Order extends React.Component {
     render() {
+        const orderIds = Object.keys(this.props.order);
+
         return (
-            <div className="order">Order!!!</div>
+            <div className="order-wrap">
+                <h2>Order</h2>
+                {orderIds}
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
         )
     }
 }
