1b1a64324097cec3b245c4d4cc9ecc21311dccd1
Jonathan Dempsey <jonathanedempsey@gmail.com>
2020-08-06 21:38:51 +0100

    Displaying Order State with JSX
    Tidying up render in Order.js

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,6 +2,17 @@ import React from "react";
 import { formatPrice } from "../helpers";
 
 class Order extends React.Component {
+    renderOrder = (key) => {
+        const fish = this.props.fishes[key];
+        const count = this.props.order[key];
+        return (
+            <li>
+                {count} lbs {fish.name}
+                {formatPrice(count * fish.price)}
+            </li>
+        );
+    };
+
     render() {
         const orderIds = Object.keys(this.props.order);
         const total = orderIds.reduce((prevTotal, key) => {
@@ -21,14 +32,12 @@ class Order extends React.Component {
         return (
             <div className="order-wrap">
                 <h2>Order</h2>
-                {orderIds}
+                <ul>{orderIds.map(this.renderOrder)}</ul>
                 <div className="total">
                     Total:
                     <strong>{formatPrice(total)}</strong>
                 </div>
-                <ul>
-                    <li></li>
-                </ul>
+
             </div>
         )
     }
