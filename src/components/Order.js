a4f696a3a73ce4f1427b6fbbfa78d0ec03c67365
Jonathan Dempsey <jonathanedempsey@gmail.com>
2020-08-06 21:47:56 +0100

    Displaying Order State with JSX
    Adding unAvailable conditional

--- src/components/Order.js
+++ src/components/Order.js
@@ -5,6 +5,16 @@ class Order extends React.Component {
     renderOrder = (key) => {
         const fish = this.props.fishes[key];
         const count = this.props.order[key];
+        const isAvailable = fish.status === 'available';
+
+        if (!isAvailable) {
+            return (
+                <li key={key}>
+                    Sorry {fish ? fish.name : "fish"} is no longer available
+                </li>
+            );
+        }
+
         return (
             <li>
                 {count} lbs {fish.name}
@@ -32,7 +42,7 @@ class Order extends React.Component {
         return (
             <div className="order-wrap">
                 <h2>Order</h2>
-                <ul>{orderIds.map(this.renderOrder)}</ul>
+                <ul className="order">{orderIds.map(this.renderOrder)}</ul>
                 <div className="total">
                     Total:
                     <strong>{formatPrice(total)}</strong>
