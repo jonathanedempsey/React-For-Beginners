f9a68d111ee084cb1e88e962f85b314f854c9d5f
Jonathan Dempsey <jonathanedempsey@gmail.com>
2020-09-15 20:49:50 +0100

    Bi-directional Data Flow and Live State Editing
    handleChange() will dynamically select the correct property to edit based on the form field that has been edited

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -1,17 +1,29 @@
 import React from "react";
 
 class EditFishForm extends React.Component {
+    handleChange = (event) => {
+        console.log(event.currentTarget.name); // eg name="price"
+        // update that fish
+        // 1. Take a copy of the current fish
+        // Dynamically choose the current property to change
+        const updateFish = {
+            ...this.props.fish,
+            [event.currentTarget.name]: event.currentTarget.value
+        };
+        console.log(updateFish);
+    }
+
     render() {
         return(
             <div className="fish-edit">
-                <input type="text" name="name" value={this.props.fish.name}></input>
-                <input type="text" name="price" value={this.props.fish.price}></input>
-                <select type="text" name="status" value={this.props.fish.status}>
+                <input type="text" name="name" onChange={this.handleChange} value={this.props.fish.name} />
+                <input type="text" name="price" onChange={this.handleChange} value={this.props.fish.price} />
+                <select type="text" name="status" onChange={this.handleChange} value={this.props.fish.status}>
                     <option value="available">Fresh</option>
                     <option value="unavailable">Sold Out</option>
                 </select>
-                <textarea type="text" name="desc" value={this.props.fish.desc} />
-                <input type="text" name="image" value={this.props.fish.image}></input>
+                <textarea type="text" name="desc" onChange={this.handleChange} value={this.props.fish.desc} />
+                <input type="text" name="image" onChange={this.handleChange} value={this.props.fish.image} />
             </div>
         );
     }
