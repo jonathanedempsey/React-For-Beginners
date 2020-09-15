275c03b6802c4cfb59b43867a7bd3dfcfa570b6c
Jonathan Dempsey <jonathanedempsey@gmail.com>
2020-09-15 20:40:04 +0100

    Bi-directional Data Flow and Live State Editing
    Pull the data for each specific fish into an edit form

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -4,14 +4,14 @@ class EditFishForm extends React.Component {
     render() {
         return(
             <div className="fish-edit">
-                <input type="text" name="name"></input>
-                <input type="text" name="price"></input>
-                <select type="text" name="status">
+                <input type="text" name="name" value={this.props.fish.name}></input>
+                <input type="text" name="price" value={this.props.fish.price}></input>
+                <select type="text" name="status" value={this.props.fish.status}>
                     <option value="available">Fresh</option>
                     <option value="unavailable">Sold Out</option>
                 </select>
-                <textarea type="text" name="desc" />
-                <input type="text" name="image"></input>
+                <textarea type="text" name="desc" value={this.props.fish.desc} />
+                <input type="text" name="image" value={this.props.fish.image}></input>
             </div>
         );
     }
