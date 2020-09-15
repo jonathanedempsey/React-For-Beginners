a7d731a73938b33d364a523c83249833ba90f3aa
Jonathan Dempsey <jonathanedempsey@gmail.com>
2020-09-15 20:34:22 +0100

    Bi-directional Data Flow and Live State Editing
    Set up bones of EditFishForm in the app

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -2,7 +2,18 @@ import React from "react";
 
 class EditFishForm extends React.Component {
     render() {
-        return <p>Edit Fish</p>;
+        return(
+            <div className="fish-edit">
+                <input type="text" name="name"></input>
+                <input type="text" name="price"></input>
+                <select type="text" name="status">
+                    <option value="available">Fresh</option>
+                    <option value="unavailable">Sold Out</option>
+                </select>
+                <textarea type="text" name="desc" />
+                <input type="text" name="image"></input>
+            </div>
+        );
     }
 }
 
