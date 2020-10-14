3c4436a856b41a6b149c84f0af263be58adc5a05
Jonathan Dempsey <jonathanedempsey@gmail.com>
2020-10-14 21:06:47 +0100

    Authentication
    Set up login buttons

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,10 @@ import React from 'react';
 const Login = () => (
     <nav className="login">
         <h2>Inventory Login</h2>
+        <p>Sign in to manage your store's inventory</p>
+        <button className="github" onClick={() => this.props.authenticate('Github')}>Log In With Github</button>
+        <button className="twitter" onClick={() => this.props.authenticate('Twitter')}>Log In With Twitter</button>
+        <button className="facebook" onClick={() => this.props.authenticate('Facebook')}>Log In With Facebook</button>
     </nav>
 );
 
